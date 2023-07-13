d0e33e1deb0a43cbb96fa025423723e62d4356cd
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-07-12 21:50:58 -0400

    Day013

---
+++ Day013/SortTheStudentsByTheirKthScore/Solution/Solution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Solution{
+    public int[][] SortTheStudents(int[][] score, int k) 
+    {
+        var list = score.ToList();
+        list.Sort((x,y) => y[k] - x[k]);
+        return list.ToArray();
+    }
+}
